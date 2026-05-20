0e7a22dda026ac8a15a224f458b460dce2e35748
Dhriti Dixit <ddhriti61@gmail.com>
2026-05-20 09:45:49 +0000

    Sync LeetCode submission Runtime - 40 ms (45.49%), Memory - 26.6 MB (95.59%)

---
+++ 0448-find-all-numbers-disappeared-in-an-array/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for num in nums:
+            index = abs(num) - 1
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+                
+        return [i + 1 for i, num in enumerate(nums) if num > 0]
+        
