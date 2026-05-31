9e3f7cc44abe9681cda3ed22b65863663dda49c3
Dhriti Dixit <ddhriti61@gmail.com>
2026-05-31 18:25:18 +0000

    Sync LeetCode submission Runtime - 99 ms (64.18%), Memory - 21.4 MB (62.04%)

---
+++ 2245-destroying-asteroids/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def asteroidsDestroyed(self, mass, asteroids):
+        """
+        :type mass: int
+        :type asteroids: List[int]
+        :type rtype: bool
+        """
+        asteroids.sort()
+        
+        for asteroid in asteroids:
+            if mass < asteroid:
+                return False
+            mass += asteroid
+            
+        return True
+        
